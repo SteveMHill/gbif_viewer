02f88affec00a4d2e02ca733a167b3b9ded04798
stevenmhill <steven.smhill@gmail.com>
2024-11-28 17:45:56 +0100

    new approach

--- dashboard.py
+++ dashboard.py
@@ -7,93 +7,188 @@ import plotly.express as px
 import plotly.figure_factory as ff
 import dash_bootstrap_components as dbc
 
+
+# the style arguments for the sidebar.
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "height": "100%",
+    "z-index": 1,
+    "overflow-x": "hidden",
+    "transition": "all 0.5s",
+    "padding": "0.5rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the style arguments for the main content page.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "4rem 1rem 2rem",
+}
+
+TEXT_STYLE = {
+    'textAlign': 'center',
+    'color': '#191970'
+}
+
+CARD_TEXT_STYLE = {
+    'textAlign': 'center',
+    'color': '#0074D9'
+}
+
 px.set_mapbox_access_token(open(".mapbox_token").read())
 
-data = pl.read_parquet('data/dragonfly_small.parquet', columns=["gbifID","occurrenceID","country","genus","decimalLatitude","decimalLongitude"])
+data = pl.read_parquet('data/dragonfly_database.parquet', columns=["gbifID","occurrenceID","country","Region","species","publisherType","genus","decimalLatitude","decimalLongitude"])
+data = data.rename({"species": "Species"})
 region = data.select(pl.col("country")).unique().to_series().to_list()
 region.append('All')
 region.sort()
 
+nxHex=[50,100,150,200,250]
+vari = ["Region","Species","publisherType"]
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-SIDEBAR_STYLE = {
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "20rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-
-controls = dbc.Card([
-            html.Div([
-                html.Img(src="./assets/gce_logo.png",style={'height':'30%', 'width':'30%'}),
-                html.H2('Dragonfly Database', style = { 'color': 'black'}),
-                html.P('Select Region:',  style = {'color': 'black'}),
-                 dcc.Dropdown(region, 'All', id='country'),
-            ]),
-        ], style = SIDEBAR_STYLE)
-
-content =  html.Div([
-                 dbc.Row([html.H4(id='number')],style={"height": "5vh"}),
-                 dbc.Row([dcc.Graph(id = 'map',config = {'displayModeBar': 'hover'})], style={"height": "70vh"}),
-                 dbc.Row([dcc.Graph(id = 'bar_line_1',config = {'displayModeBar': 'hover'})],style={"height": "25vh"}),
+controls = html.Div(
+            [
+                html.P('Select Region', style={'textAlign': 'center'}),
+                dcc.Dropdown(region,  value='All', id='country'),
+                html.Br(),
+                html.P('Variable', style={'textAlign': 'center'}),
+                dcc.Dropdown(vari,  value='Region', id='para'),
+                html.Br(),
+                html.P('HexSize', style={'textAlign': 'center'}),
+                dcc.Dropdown(nxHex, value=100, id='hexsize')
             ])
 
-app.layout = dbc.Container([
-        dbc.Row([
-            dbc.Col([html.Div([controls])], md=3),
-            dbc.Col([html.Div([content])], md=9),
-        ]),
-        ],
-        fluid=True,
-    )
+sidebar = html.Div(
+    [
+          html.H2('Parameters', style=TEXT_STYLE),
+          html.Hr(),
+          controls
+            ],style = SIDEBAR_STYLE)
+
+card_row = dbc.Row([
+    dbc.Col(
+        dbc.Card(
+            [
+                dbc.CardBody(
+                    [
+                        html.H4("Occurences",style=CARD_TEXT_STYLE),
+                        html.P(id='number', style=CARD_TEXT_STYLE),
+                    ]
+                )
+            ]
+        ),
+        md=6
+    ),
+    dbc.Col(
+        dbc.Card(
+            [
+                dbc.CardBody(
+                    [
+                        html.H4("Species",style=CARD_TEXT_STYLE),
+                        html.P(id='species_number', style=CARD_TEXT_STYLE),
+                    ]
+                )
+            ]
+        ),
+        md=6
+    ),
+])
+
+first_row = dbc.Row(
+    [
+        dbc.Col(
+             dcc.Graph(id = 'map',config = {'displayModeBar': 'hover','scrollZoom': True}), md=12,
+        )
+    ]
+)
+
+second_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(id = 'bar_line_1',config = {'displayModeBar': 'hover'}), md=12,
+        )
+    ]
+)
+
+
+content = html.Div(
+    [
+        html.H2('Dragonfly Dashboard', style=CARD_TEXT_STYLE),
+        html.Hr(),
+        card_row,
+        first_row,
+        second_row,
+    ],
+    style=CONTENT_STYLE
+)
+
+app.layout = html.Div([sidebar, content])
 
 @app.callback(
     Output(component_id='number', component_property='children'),
     Input(component_id='country', component_property='value')
 )
-def update_output_div(country):
+def update_number(country):
     if country == "All":
         df = data
         occ_number = len(df)
     else:
         df = data.filter(pl.col("country") == country)
         occ_number = len(df)
-    return f'Occurrences: {occ_number}'
+    return f'{occ_number}'
+
+@app.callback(
+    Output(component_id='species_number', component_property='children'),
+    Input(component_id='country', component_property='value')
+)
+def update_species(country):
+    if country == "All":
+        df = data
+        spc_number = len(df.unique(subset="Species"))
+    else:
+        df = data.filter(pl.col("country") == country)
+        spc_number = len(df.unique(subset="Species"))
+    return f'{spc_number}'
 
 @app.callback(Output('bar_line_1', 'figure'),
-    [Input('country', 'value')])
-
-def update_graph(country):
+    [Input('country', 'value')],
+    [Input('para', 'value')])
+def update_graph(country, para):
     if country == "All":
-        df = data
-        occ_number = len(df)
-        npc = df.group_by(pl.col("country")).len().sort(by="len", descending=True)
-        barfig = px.bar(npc, x='country', y='len',labels={'len':'Number of occurrences','country':'Country'}, log_y=True)
+        if para == "Region":
+            npc = data.group_by(pl.col("country")).len().sort(by="len", descending=True)
+            barfig = px.bar(npc, x="country", y='len',labels={'len':'Number of occurrences'}, log_y=True)
+        else:
+            npc = data.group_by(pl.col(para)).len().sort(by="len", descending=True)
+            barfig = px.bar(npc, x=para, y='len',labels={'len':'Number of occurrences'}, log_y=True)
     else:
         df = data.filter(pl.col("country") == country)
-        occ_number = len(df)
-        spc = df.group_by(pl.col("genus")).len().sort(by= "len", descending=True)
-        barfig = px.bar(spc, x='genus', y='len',labels={'len':'Number of occurrences','genus':'Genus'})
-
+        spc = df.group_by(pl.col(para)).len().sort(by= "len", descending=True)
+        barfig = px.bar(spc, x=para, y='len',labels={'len':'Number of occurrences'})
     return barfig
 
 @app.callback(Output('map', 'figure'),
-    [Input('country', 'value')])
-def update_map(country):
+    [Input('country', 'value')],
+    [Input('hexsize', 'value')])
+def update_map(country, hexsize):
     if country == "All":
         df = data
         mapfig = ff.create_hexbin_mapbox(
             data_frame=df, lat="decimalLatitude", lon="decimalLongitude",
-            nx_hexagon=150, zoom=4,opacity=0.4, range_color = [1,100],min_count= 3, labels={"color": "Point Count"},color_continuous_scale='turbo',
+            nx_hexagon=hexsize, zoom=2,opacity=0.4, range_color = [1,100],min_count= 3, labels={"color": "Point Count"},color_continuous_scale='turbo',
         )
     else:
         df = data.filter(pl.col("country") == country)
         mapfig = ff.create_hexbin_mapbox(
                 data_frame=df, lat="decimalLatitude", lon="decimalLongitude",
-                nx_hexagon=50,opacity=0.4,min_count= 1, labels={"color": "Point Count"},color_continuous_scale='turbo',
+                nx_hexagon=hexsize,opacity=0.4,min_count= 1, labels={"color": "Point Count"},color_continuous_scale='turbo',
             )
     return mapfig
 
