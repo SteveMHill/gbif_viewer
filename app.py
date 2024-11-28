fa9ebc68f59f67f8db77498d48ffc64a31f654d9
stevenmhill <steven.smhill@gmail.com>
2024-11-28 18:03:42 +0100

    token function

--- app.py
+++ app.py
@@ -6,7 +6,22 @@ import polars as pl
 import plotly.express as px
 import plotly.figure_factory as ff
 import dash_bootstrap_components as dbc
+import os
 
+
+def get_mapbox_token():
+    if os.environ.get("MAPBOX_TOKEN", None):
+        return os.environ["MAPBOX_TOKEN"]
+    elif os.path.exists(".mapbox_token"):
+        with open(".mapbox_token", "rt") as f:
+            return f.read().strip()
+    else:
+        raise ValueError(
+            "Could not load mapbox token\n"
+            "Either set the MAPBOX_TOKEN environment variable to your token string \n"
+            "or create a file named .mapbox_token containing your token in the\n"
+            "top-level project directory"
+        )
 
 # the style arguments for the sidebar.
 SIDEBAR_STYLE = {
@@ -40,7 +55,8 @@ CARD_TEXT_STYLE = {
     'color': '#0074D9'
 }
 
-px.set_mapbox_access_token(open(".mapbox_token").read())
+mt = get_mapbox_token()
+px.set_mapbox_access_token(mt)
 
 data = pl.read_parquet('data/dragonfly_database.parquet', columns=["gbifID","occurrenceID","country","Region","species","publisherType","genus","decimalLatitude","decimalLongitude"])
 data = data.rename({"species": "Species"})
